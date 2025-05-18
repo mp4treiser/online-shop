969ba15890554c0df264f065ee603441981b0a13
mp4treiser <mp4treiser@gmail.com>
2025-05-18 17:12:52 +0300

    Completed Task 7 Admin

--- src/core/settings.py
+++ src/core/settings.py
@@ -136,6 +136,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
